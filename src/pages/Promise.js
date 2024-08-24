b8e05d8a44d4e704a289221d314f4a733440801d
Arijit <bhowmikzar@gmail.com>
2024-08-24 15:16:26 +0530

    promise page added

--- src/pages/Promise.js
+++ src/pages/Promise.js
@@ -3,8 +3,83 @@ import React from "react";
 const PromiseComponent = () => {
   return (
     <div className="text-center py-16">
-      <h1 className="text-3xl font-bold">Promise Us</h1>
-      <p>Promise information for ParkWest Church.</p>
+      <div className="relative mb-16" id="who-we-are">
+        <img
+          src="/every.png"
+          alt="Background"
+          className="w-full h-96 object-cover"
+        />
+        <div className="absolute inset-0 flex items-center justify-center text-center bg-black bg-opacity-50">
+          <h1 className="text-white text-4xl md:text-6xl lg:text-8xl font-extrabold">
+            Promise Words
+          </h1>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center space-y-8 md:space-y-16">
+        <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold">
+          YEAR 2024
+        </h2>
+        <img
+          src="/p1.png"
+          alt="Word 1 Graphic"
+          className="w-100 h-48 md:h-64 lg:h-96 object-cover"
+        />
+        <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold">
+          FEBRUARY 2024
+        </h2>
+        <img
+          src="/p2.png"
+          alt="Word 2 Graphic"
+          className="w-100 h-48 md:h-64 lg:h-96 object-cover"
+        />
+        <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold">
+          MARCH 2024
+        </h2>
+        <img
+          src="/p3.png"
+          alt="Word 3 Graphic"
+          className="w-100 h-48 md:h-64 lg:h-96 object-cover"
+        />
+        <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold">
+          APRIL 2024
+        </h2>
+        <img
+          src="/p4.png"
+          alt="Word 4 Graphic"
+          className="w-100 h-48 md:h-64 lg:h-96 object-cover"
+        />
+        <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold">MAY 2024</h2>
+        <img
+          src="/p5.png"
+          alt="Word 5 Graphic"
+          className="w-100 h-48 md:h-64 lg:h-96 object-cover"
+        />
+        <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold">
+          JUNE 2024
+        </h2>
+        <img
+          src="/p6.png"
+          alt="Word 6 Graphic"
+          className="w-100 h-48 md:h-64 lg:h-96 object-cover"
+        />
+        <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold">
+          JULY 2024
+        </h2>
+        <img
+          src="/p7.png"
+          alt="Word 7 Graphic"
+          className="w-100 h-48 md:h-64 lg:h-96 object-cover"
+        />
+        <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold">
+          AUGUST 2024
+        </h2>
+        <img
+          src="/p8.png"
+          alt="Word 8 Graphic"
+          className="w-100 h-48 md:h-64 lg:h-96 object-cover"
+        />
+      </div>
     </div>
   );
 };
